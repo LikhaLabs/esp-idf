56cba0dcd23fb6ba30898d6c6814f79029f41233
Renz Bagaporo <renz.bagaporo@gmail.com>
2021-07-19 11:19:21 +0800

    f

--- examples/peripherals/uart/uart_echo/main/uart_echo_example_main.c
+++ examples/peripherals/uart/uart_echo/main/uart_echo_example_main.c
@@ -12,6 +12,7 @@
 #include "freertos/task.h"
 #include "driver/uart.h"
 #include "driver/gpio.h"
+#include "esp_log.h"
 #include "sdkconfig.h"
 
 /**
@@ -32,10 +33,33 @@
 #define ECHO_UART_BAUD_RATE     921600
 #define ECHO_TASK_STACK_SIZE    (CONFIG_EXAMPLE_TASK_STACK_SIZE)
 
+static int count = 0;
+
+// #define UART1_TX_PIN    26
+// #define UART1_RX_PIN    27
+
+// #define UART2_TX_PIN    16
+// #define UART2_RX_PIN    17
+
+#define CHANGE_MODE_PIN 14
+
+
 #define BUF_SIZE (1024)
 
 SemaphoreHandle_t printf_Mutex;
 
+int64_t change_mode_request_time = 0;
+bool    change_mode_request = false;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    int64_t time_elapsed = esp_timer_get_time() - change_mode_request_time;
+
+    if(time_elapsed > 170000) {
+        change_mode_request_time = esp_timer_get_time();
+        change_mode_request = true;
+    }
+}
 
 typedef enum {
     MONITOR,
@@ -95,6 +119,13 @@ static void echo_task(void *arg)
 
     while (1) {
 
+        printf("change not requested!\n");
+        if (change_mode_request) {
+            printf("change requested!\n");
+            change_mode_request = false;
+
+        }
+
         // Read data from the UART
         memset(data, 0, BUF_SIZE);
         int len = uart_read_bytes(uart_num, data, BUF_SIZE, 20 / portTICK_RATE_MS);
@@ -111,11 +142,19 @@ static void echo_task(void *arg)
 
 void app_main(void)
 {
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_INTR_NEGEDGE;
+    io_conf.pin_bit_mask = (1ULL<<CHANGE_MODE_PIN);
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = 1;
+    io_conf.pull_down_en = 0;
+    gpio_config(&io_conf);
+
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(CHANGE_MODE_PIN, gpio_isr_handler, (void*) CHANGE_MODE_PIN);
+
     int uart1 = UART_NUM_1, uart2 = UART_NUM_2;
-
     printf_Mutex = xSemaphoreCreateMutex();
-
     xTaskCreate(echo_task, "uart_echo_task1", ECHO_TASK_STACK_SIZE, &uart1, 10, NULL);
     xTaskCreate(echo_task, "uart_echo_task2", ECHO_TASK_STACK_SIZE, &uart2, 20, NULL);
-
 }
